68155e953487cfac1edb22f33c7ebecdc36be484
bolluSharath <73945493+bolluSharath@users.noreply.github.com>
2021-07-22 16:47:19 +0530

    Add files via upload

---
+++ Contestant.js
@@ -0,0 +1,44 @@
+class Contestant{
+    constructor(){
+        this.index = null;
+        this.answer = 0;
+        this.name = null;
+    }
+
+    getCount(){
+        var contestantCountRef = database.ref('contestantCount')
+        contestantCountRef.on("value",(data)=>{
+            contestantCount = data.val();
+        })
+    }
+    updateCount(){
+        database.ref('/').update({
+            contestantCount: count
+        });
+    }
+    update(){
+        var contestantIndex = "contestants/contestant" + this.index;
+        database.ref(contestantIndex).set({
+            name:this.name,
+            answer:this.answer
+        });
+    }
+    static getplayerInfo(){
+        var contestantInfoRef = database.ref('contestants');
+        contestantInfoRef.on("value",(data)=>{
+            allContestants = data.val();
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# the-quiz-game
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
